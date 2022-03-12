bed6e68c6a7ce3229ab0cc62cc1066e489fd7aac
frankluongo <frank@frankluongo.com>
2022-03-12 15:30:18 -0500

    update scripts

--- src/templates/functions/theme_scripts.php
+++ src/templates/functions/theme_scripts.php
@@ -1,7 +1,8 @@
 <?php
 function theme_scripts()
 {
-  wp_enqueue_style('appStyle', get_template_directory_uri() . 'assets/app.css', array(), wp_get_theme()->get('Version'));
+  wp_enqueue_style('app', get_template_directory_uri() . '/assets/app.css', array(), wp_get_theme()->get('Version'));
+  wp_enqueue_script('app', get_template_directory_uri() . '/assets/app.js');
 }
 
 add_action('wp_enqueue_scripts', 'theme_scripts');
